8acfca79d99b7c5feb8184c1d2ed618e1a05b092
Steve Van Opstal <stevevanopstal@gmail.com>
2016-03-16 11:58:35 +0100

    feat(shop): add eye icon

--- src/build/shop.component.ts
+++ src/build/shop.component.ts
@@ -8,7 +8,6 @@ import {LolApiService} from 'app/lolapi.service';
 import {ErrorComponent} from 'app/error.component';
 import {DDragonDirective} from 'app/ddragon.directive';
 
-
 @Pipe({
   name: 'translate'
 })
@@ -68,11 +67,14 @@ class TranslatePipe {
       <div class="search">
         <input type="text" name="name" placeholder="Name">
         <button type="button" name="show-disabled" title="Display hidden items">
-          <span class="icon-eye" aria-hidden="true"></span>
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+          </svg>
         </button>
       </div>
       <div class="items">
-            <div class="item" *ngFor="#item of items?.data" [ngClass]="{disabled: item.disabled}" title="{{item.description}}">
+        <div class="item" *ngFor="#item of items?.data" [ngClass]="{disabled: item.disabled}" title="{{item.description}}">
           <img [ddragon]="'item/' + item.image.full">
           <div>
             <p class="name">{{item.name}}</p>
