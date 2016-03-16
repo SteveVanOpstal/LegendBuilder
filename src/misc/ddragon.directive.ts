aa3003bdc0a92a036194bd720342486a974a3c16
Steve Van Opstal <stevevanopstal@gmail.com>
2016-03-16 10:50:54 +0100

    feat(ddragon): add sprite support

--- src/misc/ddragon.directive.ts
+++ src/misc/ddragon.directive.ts
@@ -1,6 +1,6 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
 
-import {Directive, Input, ElementRef, OnChanges, SimpleChange} from 'angular2/core';
+import {Directive, Input, ElementRef, OnChanges} from 'angular2/core';
 
 import {LolApiService} from 'app/lolapi.service';
 
@@ -10,6 +10,8 @@ import {LolApiService} from 'app/lolapi.service';
 
 export class DDragonDirective implements OnChanges {
   @Input('ddragon') image: string;
+  @Input('ddragon.x') x: number = -1;
+  @Input('ddragon.y') y: number = -1;
 
   private default: string = "/images/hourglass.svg";
   private realm: any;
@@ -20,15 +22,22 @@ export class DDragonDirective implements OnChanges {
   }
 
   updateElement(realm: any) {
-    if (this.el.nativeElement.tagName == "IMG") {
+    if (this.x >=0 && this.y >= 0) {
+      this.el.nativeElement.setAttribute("style", this.buildStyle(this.image, realm, this.x, this.y));
+    }
+    else if (this.el.nativeElement.tagName == "IMG") {
       this.el.nativeElement.setAttribute("src", this.buildUrl(this.image, realm));
     }
     else {
       this.el.nativeElement.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', this.buildUrl(this.image, realm));
     }
   }
+  
+  buildStyle(image: string, realm: any, x: number, y: number): string {
+    return "background:url(" + this.buildUrl(image, realm) + ") " + x + "px " + y + "px;";
+  }
 
-  buildUrl(image: string, realm: any) {
+  buildUrl(image: string, realm: any): string {
     if (!this.image || !realm) {
       return this.default;
     }
