935fb166423edcc4795952059921595c4ee90230
Steve Van Opstal <stevevanopstal@gmail.com>
2016-03-24 17:20:38 +0100

    test(ability-sequence): add test structure

---
+++ src/app/build/ability-sequence.component.spec.ts
@@ -0,0 +1,28 @@
+import {provide} from 'angular2/core';
+import {BaseRequestOptions, Http, Response, ResponseOptions} from 'angular2/http';
+import {RouteParams} from 'angular2/router';
+import {RootRouter} from 'angular2/src/router/router';
+
+import {it, inject, beforeEachProviders} from 'angular2/testing';
+import {MockBackend, MockConnection} from 'angular2/http/testing';
+
+import {LolApiService} from '../misc/lolapi.service';
+import {AbilitySequenceComponent} from './ability-sequence.component';
+
+describe('AbilitySequenceComponent', () => {
+  beforeEachProviders(() => [
+    provide(RouteParams, { useValue: new RouteParams({ region: 'euw' }) }),
+    BaseRequestOptions,
+    MockBackend,
+    provide(Http, {
+      useFactory: function(backend, defaultOptions) {
+        return new Http(backend, defaultOptions);
+      },
+      deps: [MockBackend, BaseRequestOptions]
+    }),
+
+    LolApiService,
+    AbilitySequenceComponent
+  ]);
+  
+});
