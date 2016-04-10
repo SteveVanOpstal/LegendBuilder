7e6241ac7be91fb16514e232f3032d6658714844
SteveVanOpstal <stevevanopstal@gmail.com>
2016-04-10 17:41:30 +0200

    test(spec-bundle): fix zone require

--- spec-bundle.js
+++ spec-bundle.js
@@ -2,7 +2,7 @@ Error.stackTraceLimit = Infinity;
 
 require('core-js');
 
-require('zone.js/dist/zone.js');
+require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone.js');
 require('zone.js/dist/jasmine-patch.js');
 
