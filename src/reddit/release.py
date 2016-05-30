c0aaa9c61167a2318f485ffa4aa9639507beded3
Steve Van Opstal <stevevanopstal@gmail.com>
2016-05-30 13:43:31 +0200

    build(reddit): add header and footer

--- src/reddit/release.py
+++ src/reddit/release.py
@@ -4,7 +4,8 @@ import re
 from settings import settings
 
 CHANGELOG_LOCATION = './CHANGELOG.md'
-MESSAGE_LOCATION = './src/reddit/message.md'
+HEADER_LOCATION = './src/reddit/header.md'
+FOOTER_LOCATION = './src/reddit/footer.md'
 
 reddit = praw.Reddit(user_agent='LegendBuilderBot by /u/%s' % (settings['REDDIT_USERNAME']))
 reddit.login(settings['REDDIT_USERNAME'], settings['REDDIT_PASSWORD'], disable_warning=True)
@@ -15,13 +16,13 @@ with open('package.json') as packageFile:
 title = 'Legend Builder v%s' % (package['version'])
 print('Submitting \'%s\' to %s' % (title, settings['SUBREDDIT']))
 
-with open(CHANGELOG_LOCATION, 'r') as changelog, open(MESSAGE_LOCATION, 'r') as message:
+with open(CHANGELOG_LOCATION, 'r') as changelog, open(HEADER_LOCATION, 'r') as header, open(FOOTER_LOCATION, 'r') as footer:
   # add changelog
   content = str(changelog.read())
-  # remove links
+  # remove link tags
   content = re.sub(r'<a name=".*?"></a>', '', content);
-  # prefix with message
-  content = str(message.read()) + content;
+  # prefix with header, postfix with footer
+  content = str(header.read()) + '\n*****\n' + content + '\n*****\n' + str(footer.read());
   
   submission = reddit.submit(settings['SUBREDDIT'], title, text=content, send_replies=True)
   submission.sticky(bottom=False)
