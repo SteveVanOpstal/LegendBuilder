41fe7ada42237f794c19e1630d36bc167d225fbf
Steve Van Opstal <stevevanopstal@gmail.com>
2016-07-05 16:47:26 +0200

    test(item.component): provide MockElementRef

--- src/client/build/items/item.component.spec.ts
+++ src/client/build/items/item.component.spec.ts
@@ -1,9 +1,12 @@
+import {ElementRef} from '@angular/core';
+
 import {beforeEachProviders, inject, it} from '@angular/core/testing';
+import {MockElementRef} from '../../testing';
 
 import {ItemComponent} from './item.component';
 
 describe('ItemComponent', () => {
-  beforeEachProviders(() => [ItemComponent]);
+  beforeEachProviders(() => [ItemComponent, {provide: ElementRef, useValue: new MockElementRef()}]);
 
   it('should be initialised', inject([ItemComponent], (component) => {
        expect(component.item).not.toBeDefined();
