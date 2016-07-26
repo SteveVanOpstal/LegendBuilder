30a4186c18bcfbcc80cacab62c62ddd7e6b3eb09
SteveVanOpstal <stevevanopstal@gmail.com>
2016-07-27 00:31:35 +0200

    build(sauce): optional tunnel creation

--- config/build.js
+++ config/build.js
@@ -4,6 +4,9 @@ let spawnSync = require('child_process').spawnSync;
 let async = require('async');
 let browsers = require('./browser-providers.conf.js');
 
+var SauceTunnel = require('sauce-tunnel');
+var tunnel = new SauceTunnel(process.env.SAUCE_USERNAME, process.env.SAUCE_ACCESSKEY, process.env.TRAVIS_JOB_NUMBER);
+let tunnelOpen = false; 
 
 /* configuration */
 
@@ -15,11 +18,13 @@ switch (process.env.CI_MODE) {
     break;
 
   case 'client_test_required':
+    open_sauce_tunnel();
     scripts.push('test:client -- --browsers=' + browsers.sauceAliases.CI_REQUIRED.join(','));
     scripts.push('coveralls');
     break;
 
   case 'client_test_optional':
+    open_sauce_tunnel();
     scripts.push('test:client -- --browsers=' + browsers.sauceAliases.CI_OPTIONAL.join(','));
     break;
 
@@ -44,7 +49,8 @@ switch (process.env.CI_MODE) {
 /* execute */
 
 execute_scripts(scripts);
-
+close_sauce_tunnel();
+  
 function execute_scripts(scripts) {
   for (let script of scripts) {
     spawn_process(script);
@@ -71,3 +77,26 @@ function spawn_process(script) {
     process.exit(1);
   }
 }
+
+
+/* sauce-tunnel */
+
+function open_sauce_tunnel() {
+  console.log('Sauce tunnel: starting..');
+  tunnel.start(function(status){
+    if (!status) {
+      console.log('Sauce tunnel: start failed');
+      process.exit(1);
+    } else {
+      tunnelOpen = true;
+    }
+  });
+}
+
+function close_sauce_tunnel() {
+  if (tunnelOpen) {
+    tunnel.stop(function () {
+      console.log('Sauce tunnel: stopped');
+    });
+  }
+} 
--- config/karma.conf.js
+++ config/karma.conf.js
@@ -59,7 +59,6 @@ module.exports = function(config) {
   if (process.env.TRAVIS) {
     config.sauceLabs.build =
         'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
-    config.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
 
     console.log('>>>> setting socket.io transport to polling <<<<');
     config.transports = ['polling'];
