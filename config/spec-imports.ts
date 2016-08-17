9a7a289e78ef0f4b553efb89cc6b71f19cf7e09f
Steve Van Opstal <stevevanopstal@gmail.com>
2016-08-17 09:43:44 +0200

    fix(zone.js): imports incorrect

--- config/spec-imports.ts
+++ config/spec-imports.ts
@@ -1,10 +1,11 @@
 import 'core-js';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/proxy-zone';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
 import 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
 import 'rxjs/Rx';
 import './jasmine-matchers.js';
 
