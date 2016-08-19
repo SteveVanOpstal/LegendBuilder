b7eaa684e3537eef6a889673482b6b8a2adb4b03
Steve Van Opstal <stevevanopstal@gmail.com>
2016-08-19 09:27:01 +0200

    chore(build): check in pipes

---
+++ src/client/build/pipes/name.pipe.ts
@@ -0,0 +1,19 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'name'})
+
+export class NamePipe implements PipeTransform {
+  transform(champions: Array<any>, name) {
+    if (!champions || !name || typeof name !== 'string') {
+      return champions;
+    }
+
+    return champions.filter((champion) => {
+      return this.clean(champion.name).indexOf(this.clean(name)) === 0;
+    });
+  }
+
+  private clean(value: string): string {
+    return value.toLowerCase().replace('\'', '');
+  }
+}
