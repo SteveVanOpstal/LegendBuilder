e578f51ec42bef96f2e870fc31c885ba5b15aa3e
SteveVanOpstal <stevevanopstal@gmail.com>
2016-09-22 00:14:57 +0200

    chore(license): set correct GPL version

--- src/client/boot.ts
+++ src/client/boot.ts
@@ -1,11 +1,10 @@
 /*
-  Copyright 2015-2016 Steve Van Opstal https://github.com/SteveVanOpstal
-
   Legend Builder - An advanced League Of Legends champion builder
+  Copyright (C) 2016 Steve Van Opstal (https://github.com/SteveVanOpstal)
 
-  This program is free software; you can redistribute it and/or modify
+  This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
+  the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
   This program is distributed in the hope that it will be useful,
@@ -13,9 +12,8 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import {enableProdMode} from '@angular/core';
