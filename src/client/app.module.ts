ea34270d7f9005a610d2ba6e1772db3a4edc2d24
Steve Van Opstal <stevevanopstal@gmail.com>
2016-10-13 23:52:11 +0200

    refactor(routing): module for routes

--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -2,13 +2,12 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
 import {AUTH_PROVIDERS} from 'angular2-jwt';
 
 import {AboutModule} from './about/about.module';
 import {ActionsComponent} from './actions.component';
 import {AppComponent} from './app.component';
-import {ROUTES} from './app.routes';
+import {AppRoutingModule} from './app.routing';
 import {AssetsModule} from './assets/assets.module';
 import {BuildModule} from './build/build.module';
 import {ChampionModule} from './champion/champion.module';
@@ -25,9 +24,9 @@ import {SummonerModule} from './summoner/summoner.module';
   providers: [LolApiService, AUTH_PROVIDERS, AuthService],
   declarations: [AppComponent, ActionsComponent],
   imports: [
-    BrowserModule, CommonModule, RouterModule.forRoot(ROUTES), HttpModule, AssetsModule,
-    SharedModule, AboutModule, BuildModule, ChampionModule, LoginModule, MainModule, RegionModule,
-    SignupModule, SummonerModule
+    BrowserModule, CommonModule, AppRoutingModule, HttpModule, AssetsModule, SharedModule,
+    AboutModule, BuildModule, ChampionModule, LoginModule, MainModule, RegionModule, SignupModule,
+    SummonerModule
   ],
   bootstrap: [AppComponent, ActionsComponent]
 })
---
+++ src/client/app.routing.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {AboutComponent} from './about/about.component';
+import {BuildComponent} from './build/build.component';
+import {ChampionComponent} from './champion/champion.component';
+import {LoginComponent} from './login/login.component';
+import {MainComponent} from './main/main.component';
+import {RegionComponent} from './region/region.component';
+import {SignupComponent} from './signup/signup.component';
+import {SummonerComponent} from './summoner/summoner.component';
+
+
+@NgModule({
+  imports: [RouterModule.forRoot([
+    {path: '', component: MainComponent}, {path: 'about', component: AboutComponent},
+    {path: 'login', component: LoginComponent}, {path: 'signup', component: SignupComponent},
+    {path: 'build', component: RegionComponent},
+    {path: 'build/:region', component: SummonerComponent},
+    {path: 'build/:region/:summoner', component: ChampionComponent},
+    {path: 'build/:region/:summoner/:champion', component: BuildComponent}
+  ])],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
