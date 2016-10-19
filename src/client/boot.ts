52d88f241730b8d5b2fd014b8c2072f509c12723
Steve Van Opstal <stevevanopstal@gmail.com>
2016-10-19 10:25:15 +0200

    refactor(boot): remove file extension

--- src/client/boot.ts
+++ src/client/boot.ts
@@ -18,7 +18,7 @@
 
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module.ts';
+import {AppModule} from './app.module';
 
 if (ENV === 'production') {
   enableProdMode();
