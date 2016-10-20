56dd9a6db0b9958f8eb2c605757efa2ea1a405e1
Steve Van Opstal <stevevanopstal@gmail.com>
2016-10-20 13:24:32 +0200

    refactor: remove unused help component

--- src/client/shared/shared.module.ts
+++ src/client/shared/shared.module.ts
@@ -6,20 +6,19 @@ import {AssetsModule} from '../assets/assets.module';
 import {CapitalizePipe} from './capitalize.pipe';
 import {DDragonDirective} from './ddragon.directive';
 import {ErrorComponent} from './error.component';
-import {HelpComponent} from './help.component';
 import {LoadingComponent} from './loading.component';
 import {RetryComponent} from './retry.component';
 import {ToIterablePipe} from './to-iterable.pipe';
 
 @NgModule({
   declarations: [
-    CapitalizePipe, DDragonDirective, ErrorComponent, HelpComponent, LoadingComponent,
-    RetryComponent, ToIterablePipe
+    CapitalizePipe, DDragonDirective, ErrorComponent, LoadingComponent, RetryComponent,
+    ToIterablePipe
   ],
   imports: [CommonModule, AssetsModule],
   exports: [
-    CapitalizePipe, DDragonDirective, ErrorComponent, HelpComponent, LoadingComponent,
-    RetryComponent, ToIterablePipe
+    CapitalizePipe, DDragonDirective, ErrorComponent, LoadingComponent, RetryComponent,
+    ToIterablePipe
   ]
 })
 export class SharedModule {
