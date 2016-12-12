6502c3e18c6318ac9e2dc6e103d32da2b5d8df4b
Steve Van Opstal <stevevanopstal@gmail.com>
2016-12-12 11:46:22 +0100

    build(helpers): current directory to root directory

--- config/helpers.js
+++ config/helpers.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [__dirname].concat(args));
+  return path.join.apply(path, [__dirname + '\\..'].concat(args));
 }
 
 function merge(src, target) {
