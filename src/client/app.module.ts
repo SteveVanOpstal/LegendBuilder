9b22df644167cfc4c16cfb56e1dcd184fbf54cdf
Steve Van Opstal <stevevanopstal@gmail.com>
2017-01-11 11:06:00 +0100

    feat(auth): remove incomplete authorization feature

--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -2,33 +2,27 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
-import {AUTH_PROVIDERS} from 'angular2-jwt';
 
 import {AboutModule} from './about/about.module';
-// import {ActionsComponent} from './actions.component';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routing';
 import {AssetsModule} from './assets/assets.module';
 import {BuildModule} from './build/build.module';
 import {ChampionModule} from './champion/champion.module';
-import {LoginModule} from './login/login.module';
 import {MainModule} from './main/main.module';
 import {RegionModule} from './region/region.module';
-import {AuthService} from './services/auth.service';
 import {LolApiService} from './services/lolapi.service';
 import {SharedModule} from './shared/shared.module';
-import {SignupModule} from './signup/signup.module';
 import {SummonerModule} from './summoner/summoner.module';
 
 @NgModule({
-  providers: [LolApiService, AUTH_PROVIDERS, AuthService],
-  declarations: [AppComponent /*, ActionsComponent*/],
+  providers: [LolApiService],
+  declarations: [AppComponent],
   imports: [
     BrowserModule, CommonModule, AppRoutingModule, HttpModule, AssetsModule, SharedModule,
-    AboutModule, BuildModule, ChampionModule, LoginModule, MainModule, RegionModule, SignupModule,
-    SummonerModule
+    AboutModule, BuildModule, ChampionModule, MainModule, RegionModule, SummonerModule
   ],
-  bootstrap: [AppComponent /*, ActionsComponent*/]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
