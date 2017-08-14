2722c138eeb11bc78e6aedd39f20221384a6f26f
Steve Van Opstal <SteveVanOpstal@users.noreply.github.com>
2017-08-14 16:58:35 +0200

    feat(summoner): add loading icon

--- src/client/summoner/summoner.component.ts
+++ src/client/summoner/summoner.component.ts
@@ -14,29 +14,35 @@ import {LolApiService} from '../services';
         <input type="text" name="name" #summoner (keyup.enter)="getAccountId(summoner)">
         <button (click)="getAccountId(summoner)">Go</button>
       </p>
-      <p *ngIf="error">Error summoner does not exist</p>
+      <lb-error [error]="error" [message]="'Summoner does not exist'"></lb-error>
+      <lb-icon-load *ngIf="loading"></lb-icon-load>
     </div>`
 })
 
 export class SummonerComponent {
   error: boolean = false;
+  loading: boolean = false;
 
   constructor(
       private route: ActivatedRoute, private router: Router, private lolApi: LolApiService) {}
 
   getAccountId(event: HTMLInputElement) {
+    this.loading = true;
     this.lolApi.getAccountId(event.value)
         .subscribe(
             res => {
               if (!isNaN(res)) {
                 this.router.navigate([event.value], {relativeTo: this.route}).catch(() => {
+                  this.loading = false;
                   this.error = true;
                 });
               } else {
+                this.loading = false;
                 this.error = true;
               }
             },
             () => {
+              this.loading = false;
               this.error = true;
             });
   }
--- src/client/summoner/summoner.module.ts
+++ src/client/summoner/summoner.module.ts
@@ -1,9 +1,16 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 
+import {AssetsModule} from '../assets/assets.module';
+import {SharedModule} from '../shared/shared.module';
+
 import {SummonerComponent} from './summoner.component';
 
-@NgModule(
-    {declarations: [SummonerComponent], imports: [CommonModule], exports: [SummonerComponent]})
+
+@NgModule({
+  declarations: [SummonerComponent],
+  imports: [CommonModule, AssetsModule, SharedModule],
+  exports: [SummonerComponent]
+})
 export class SummonerModule {
 }
