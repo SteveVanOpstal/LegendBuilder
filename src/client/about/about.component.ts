3d9ff1132a66ff77079c2a9dab7b22eb0a3a6dfe
SteveVanOpstal <stevevanopstal@gmail.com>
2017-09-18 23:56:35 +0200

    feat(about): remove subscription

--- src/client/about/about.component.ts
+++ src/client/about/about.component.ts
@@ -1,104 +1,99 @@
 import {Component} from '@angular/core';
 import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'lb-about',
   styleUrls: ['./about.component.scss'],
   template: `
-    <section>
-      <h2>About</h2>
-      <p>
-        Legend Builder is an
-        <a href="https://github.com/SteveVanOpstal/LegendBuilder">open source</a>
-        champion builder for <a href="http://leagueoflegends.com">League of Legends</a>.
-      </p>
-      <p>
-        There is no guarantee that the software will fulfill your champion building dreams,
-        but with your help we can get it there.
-      </p>
-      <ul>
-        <li>
-          <span>You can <b>report a bug</b></span> by entering an
-          <a href="https://github.com/SteveVanOpstal/LegendBuilder/issues">issue</a>
-        </li>
-        <li>
-          <span>You can <b>suggest a feature</b></span> by entering an
-          <a href="https://github.com/SteveVanOpstal/LegendBuilder/issues">issue</a>
-        </li>
-        <li>
-          <span>You can <b>fix a bug</b></span> by entering a
-          <a href="https://github.com/SteveVanOpstal/LegendBuilder/pulls">pull request</a>.
-          It is advised to first enter an
-          <a href="https://github.com/SteveVanOpstal/LegendBuilder/issues">issue</a>
-          which describes the bug.
-        </li>
-        <li>
-          <span>You can <b>implement a feature</b></span> by entering a
-          <a href="https://github.com/SteveVanOpstal/LegendBuilder/pulls">pull request</a>.
-          It is advised to first enter an
-          <a href="https://github.com/SteveVanOpstal/LegendBuilder/issues">issue</a>
-          which describes the feature.
-        </li>
-      </ul>
-    </section>
-    <section *ngIf="ready">
-      <h2>Contributors</h2>
-      <a class="contributor"
-         *ngFor="let contributor of contributors"
-         [attr.href]="contributor.html_url">
-        <img alt="avatar" [attr.src]="contributor.avatar_url"/>
-        <p>{{ contributor.login }}</p>
-      </a>
-    </section>
-    <section>
-      <h2>Legal</h2>
-      <p>Legend Builder - An advanced League Of Legends champion builder</p>
-      <p>Copyright &copy; 2016 <a href="https://github.com/SteveVanOpstal">Steve Van Opstal</a></p>
-      <p>&nbsp;</p>
-      <p>
-        This program is free software: you can redistribute it and/or modify it under the terms
-        of the GNU General Public License as published by the Free Software Foundation,
-      </p>
-      <p>
-        either version 3 of the License, or (at your option) any later version. This program is
-        distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-      </p>
-      <p>
-        without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-      </p>
-      <p>
-        See the
-        <a href="https://github.com/SteveVanOpstal/LegendBuilder/blob/master/LICENSE">
-          GNU General Public License
+    <div class="content">
+      <section>
+        <h2>About</h2>
+        <p>
+          Legend Builder is an
+          <a href="https://github.com/SteveVanOpstal/LegendBuilder">open source</a>
+          champion builder for <a href="http://leagueoflegends.com">League of Legends</a>.
+        </p>
+        <p>
+          There is no guarantee that the software will fulfill your champion building dreams,
+          but with your help we can get it there.
+        </p>
+        <ul>
+          <li>
+            <span>You can <b>report a bug</b></span> by entering an
+            <a href="https://github.com/SteveVanOpstal/LegendBuilder/issues">issue</a>
+          </li>
+          <li>
+            <span>You can <b>suggest a feature</b></span> by entering an
+            <a href="https://github.com/SteveVanOpstal/LegendBuilder/issues">issue</a>
+          </li>
+          <li>
+            <span>You can <b>fix a bug</b></span> by entering a
+            <a href="https://github.com/SteveVanOpstal/LegendBuilder/pulls">pull request</a>.
+            It is advised to first enter an
+            <a href="https://github.com/SteveVanOpstal/LegendBuilder/issues">issue</a>
+            which describes the bug.
+          </li>
+          <li>
+            <span>You can <b>implement a feature</b></span> by entering a
+            <a href="https://github.com/SteveVanOpstal/LegendBuilder/pulls">pull request</a>.
+            It is advised to first enter an
+            <a href="https://github.com/SteveVanOpstal/LegendBuilder/issues">issue</a>
+            which describes the feature.
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Contributors</h2>
+        <a class="contributor"
+          *ngFor="let contributor of contributors | async"
+          [attr.href]="contributor.html_url">
+          <img alt="avatar" [attr.src]="contributor.avatar_url"/>
+          <p>{{ contributor.login }}</p>
         </a>
-         for more details.
-      </p>
-      <p>&nbsp;</p>
-      <p>
-        Legend Builder isn’t endorsed by Riot Games and doesn’t reflect the views or opinions
-        of Riot Games or anyone officially involved in producing or managing League of Legends.
-      </p>
-      <p>
-        League of Legends and Riot Games are trademarks or registered trademarks of Riot Games,
-        Inc. League of Legends © Riot Games, Inc.
-      </p>
-    </section>`
+      </section>
+      <section>
+        <h2>Legal</h2>
+        <p>Legend Builder - An advanced League Of Legends champion builder</p>
+        <p>Copyright &copy; 2016 <a href="https://github.com/SteveVanOpstal">Steve Van Opstal</a></p>
+        <p>&nbsp;</p>
+        <p>
+          This program is free software: you can redistribute it and/or modify it under the terms
+          of the GNU General Public License as published by the Free Software Foundation,
+        </p>
+        <p>
+          either version 3 of the License, or (at your option) any later version. This program is
+          distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+        </p>
+        <p>
+          without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+        </p>
+        <p>
+          See the
+          <a href="https://github.com/SteveVanOpstal/LegendBuilder/blob/master/LICENSE">
+            GNU General Public License
+          </a>
+          for more details.
+        </p>
+        <p>&nbsp;</p>
+        <p>
+          Legend Builder isn’t endorsed by Riot Games and doesn’t reflect the views or opinions
+          of Riot Games or anyone officially involved in producing or managing League of Legends.
+        </p>
+        <p>
+          League of Legends and Riot Games are trademarks or registered trademarks of Riot Games,
+          Inc. League of Legends © Riot Games, Inc.
+        </p>
+      </section>
+    </div>`
 })
 
 export class AboutComponent {
-  ready = false;
-  contributors: any;
+  contributors: Observable<any>;
 
   constructor(private http: Http) {
-    this.http.get('https://api.github.com/repos/SteveVanOpstal/LegendBuilder/contributors')
-        .map(res => res.json())
-        .subscribe(
-            res => {
-              this.contributors = res;
-            },
-            null,
-            () => {
-              this.ready = true;
-            });
+    this.contributors =
+        this.http.get('https://api.github.com/repos/SteveVanOpstal/LegendBuilder/contributors')
+            .map(res => res.json());
   }
 }
