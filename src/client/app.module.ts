d10d44168a781e7a62c5124359284b77205c4823
SteveVanOpstal <stevevanopstal@gmail.com>
2017-09-19 22:29:40 +0200

    fix(app): remove unused imports

--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -3,27 +3,16 @@ import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
 
-import {AboutModule} from './about/about.module';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routing';
-import {AssetsModule} from './assets/assets.module';
-import {BuildModule} from './build/build.module';
-import {ChampionsModule} from './champion/champions.module';
 import {MainModule} from './main/main.module';
-import {RegionModule} from './region/region.module';
 import {LolApiService} from './services';
-import {SharedModule} from './shared/shared.module';
-import {SummonerModule} from './summoner/summoner.module';
 import {VersionComponent} from './version.component';
 
 @NgModule({
   providers: [LolApiService],
   declarations: [AppComponent, VersionComponent],
-  imports: [
-    BrowserModule, CommonModule, AppRoutingModule, HttpModule, AssetsModule, SharedModule,
-    AboutModule, BuildModule, ChampionsModule, MainModule, RegionModule, SummonerModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpModule, MainModule],
   bootstrap: [AppComponent, VersionComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
