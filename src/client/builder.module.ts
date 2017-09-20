667662a41092eed94013a6a3612827f64591dc21
SteveVanOpstal <stevevanopstal@gmail.com>
2017-09-20 22:52:59 +0200

    refactor(builder): move child modules to the correct module

--- src/client/builder.module.ts
+++ src/client/builder.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
 
+import {BuildModule} from './build/build.module';
 import {BuilderRoutingModule} from './builder.routing';
+import {ChampionsModule} from './champion/champions.module';
+import {RegionModule} from './region/region.module';
+import {SummonerModule} from './summoner/summoner.module';
 
-@NgModule({imports: [BuilderRoutingModule]})
-export class BuilderModule {
-}
+@NgModule(
+    {imports: [BuildModule, BuilderRoutingModule, ChampionsModule, RegionModule, SummonerModule]})
+export class BuilderModule {}
--- src/client/builder.routing.ts
+++ src/client/builder.routing.ts
@@ -1,14 +1,10 @@
 import {NgModule} from '@angular/core';
-import {NoPreloading, RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 import {BuildComponent} from './build/build.component';
-import {BuildModule} from './build/build.module';
 import {ChampionsComponent} from './champion/champions.component';
-import {ChampionsModule} from './champion/champions.module';
 import {RegionComponent} from './region/region.component';
-import {RegionModule} from './region/region.module';
 import {SummonerComponent} from './summoner/summoner.component';
-import {SummonerModule} from './summoner/summoner.module';
 
 export const routes: Routes = [
   {path: '', component: RegionComponent}, {path: ':region', component: SummonerComponent},
@@ -16,10 +12,5 @@ export const routes: Routes = [
   {path: ':region/:summoner/:champion', component: BuildComponent}
 ];
 
-@NgModule({
-  imports:
-      [RouterModule.forChild(routes), BuildModule, ChampionsModule, RegionModule, SummonerModule],
-  exports: [RouterModule]
-})
-export class BuilderRoutingModule {
-}
+@NgModule({imports: [RouterModule.forChild(routes)], exports: [RouterModule]})
+export class BuilderRoutingModule {}
