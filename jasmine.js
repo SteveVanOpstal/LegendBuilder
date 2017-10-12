fa6352d7dff29a914b91f669a90ba316121d825c
SteveVanOpstal <stevevanopstal@gmail.com>
2017-10-13 01:04:36 +0200

    refactor: remove jasmine.js

--- jasmine.js
+++
@@ -1,8 +0,0 @@
-let Jasmine = require('jasmine');
-let jasmine = new Jasmine();
-
-require('./jasmine-matchers.js');
-
-jasmine.loadConfig({spec_dir: 'build/spec/server', spec_files: ['main.spec.js']});
-
-jasmine.execute();
