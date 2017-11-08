2fb57e17f2ac7527e6c8251749915f1c17d80949
Steve Van Opstal <stevevanopstal@gmail.com>
2017-11-08 11:16:13 +0100

    feat: improve sauce scripts

--- config/build/sauce-connect.js
+++ config/build/sauce-connect.js
@@ -1,44 +1,62 @@
-var sauceConnect = require('node-sauce-connect');
-var childProcess = require('child_process');
-let fs = require('fs');
+let sauce = require('node-sauce-connect');
+let http = require('http');
 
-create_dir('build');
-create_dir('build/log');
+const PORT = process.env.SAUCE_CONNECT_OPEN || 3500;
 
-function logger(data) {
+let server = http.createServer();
+let running = false;
+
+function close() {
+  if (server) {
+    console.log('Closing..');
+    server.close(() => {
+      sauce.stop();
+      console.log('Closed');
+    });
+    server = undefined;
+  }
+}
+
+server.on('request', (req, res) => {
+  let rawData = '';
+  req.on('data', (chunk) => {
+    rawData += chunk;
+  });
+  req.on('end', () => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    if (rawData === 'running?') {
+      res.end(running ? 'yes' : 'no');
+    } else {
+      res.end('okay');
+      close();
+    }
+  });
+});
+
+sauce.start([]);
+console.log('Starting..');
+
+function data(data) {
   data = data.toString();
+  if (/Error/.test(data)) {
+    error(data);
+  } else {
+    process.stdout.write('node-sauce-connect: ' + data.toString());
+  }
   if (/Sauce Connect is up, you may start your tests./.test(data)) {
-    write_pid('build/log/' + process.env.BUILD + '.pid');
-  }
-  console.log(data);
-}
-
-var args = [];
-
-module.exports = {
-  sauce_connect: () => {
-    sauceConnect.start(args);
-    console.log('starting');
-
-    sauceConnect.defaultInstance.stdout.on('data', logger);
-    sauceConnect.defaultInstance.on('data', logger);
-  }
-};
-
-
-function create_dir(path) {
-  try {
-    fs.mkdirSync(path);
-  } catch (e) {
-    if (e.code !== 'EEXIST') throw e;
+    running = true;
   }
 }
 
-function write_pid(path) {
-  fd = fs.openSync(path, 'w');
-  fs.writeSync(fd, process.pid);
+function error(error) {
+  process.stdout.write('node-sauce-connect: ' + error.toString());
+  close();
 }
 
-if (require.main === module) {
-  module.exports.sauce_connect();
-}
+sauce.defaultInstance.stdout.on('data', data);
+sauce.defaultInstance.on('data', data);
+sauce.defaultInstance.stdout.on('error', error);
+sauce.defaultInstance.on('error', error);
+
+server.listen(PORT);
+console.log('listening on port: ' + PORT);
