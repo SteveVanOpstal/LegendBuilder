eed043b062003112b27c330b34b640cd707fcd2c
SteveVanOpstal <stevevanopstal@gmail.com>
2017-11-12 17:20:50 +0100

    build(sauce): sauce-connect-launcher

--- config/build/sauce-connect.js
+++ config/build/sauce-connect.js
@@ -1,21 +1,55 @@
-let sauce = require('node-sauce-connect');
+let sauceConnectLauncher = require('sauce-connect-launcher');
 let http = require('http');
 
 const PORT = process.env.SAUCE_CONNECT_OPEN || 3500;
 
 let server = http.createServer();
 let running = false;
+let sauce_process;
 
 function close() {
   if (server) {
     console.log('Closing..');
     server.close(() => {
-      sauce.stop();
+      if (sauce_process) {
+        sauce_process.kill();
+      }
       console.log('Closed');
     });
+    process.exit();
     server = undefined;
   }
 }
+
+console.log('Starting..');
+
+sauceConnectLauncher(
+    {
+      tunnelIdentifier: process.env.TUNNEL_IDENTIFIER,
+      verbose: true,
+      noSslBumpDomains: 'all',  // android runs into certificate errors due to SSL bumping,
+                                // this argument disables SSL bumping on all domains
+      logger: (message) => {
+        console.log('node-sauce-connect: ' + message);
+      }
+    },
+    (err, sauce) => {
+      if (sauce_process) {
+        sauce_process = sauce;
+        sauce_process.on('exit', (status) => {
+          console.log('SauceLabs: stopped (' + status + ')');
+          close();
+        });
+      }
+
+      if (err) {
+        console.log('SauceLabs: start failed, \'' + err.message + '\'');
+        close();
+      } else {
+        running = true;
+        console.log('SauceLabs: running');
+      }
+    });
 
 server.on('request', (req, res) => {
   let rawData = '';
@@ -32,31 +66,6 @@ server.on('request', (req, res) => {
     }
   });
 });
-
-sauce.start(['--tunnel-identifier', process.env.TUNNEL_IDENTIFIER, '--no-ssl-bump-domains', 'all']);
-console.log('Starting..');
-
-function data(data) {
-  data = data.toString();
-  if (/Error/.test(data)) {
-    error(data);
-  } else {
-    process.stdout.write('node-sauce-connect: ' + data.toString());
-  }
-  if (/Sauce Connect is up, you may start your tests./.test(data)) {
-    running = true;
-  }
-}
-
-function error(error) {
-  process.stdout.write('node-sauce-connect: ' + error.toString());
-  close();
-}
-
-sauce.defaultInstance.stdout.on('data', data);
-sauce.defaultInstance.on('data', data);
-sauce.defaultInstance.stdout.on('error', error);
-sauce.defaultInstance.on('error', error);
 
 server.listen(PORT);
 console.log('listening on port: ' + PORT);
