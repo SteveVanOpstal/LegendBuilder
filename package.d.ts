462c5994598d873668e100e67090d6b5defee0f4
SteveVanOpstal <stevevanopstal@gmail.com>
2017-11-19 15:04:13 +0100

    refactor(version): add definition file for package

---
+++ package.d.ts
@@ -0,0 +1,3 @@
+export declare module pkg {
+  let version: string;
+}
--- src/client/version.component.ts
+++ src/client/version.component.ts
@@ -1,13 +1,13 @@
 import {Component} from '@angular/core';
-import pkg from '../../package';
+import {pkg} from '../../package';
 
 @Component({
   selector: 'lb-version',
   styleUrls: ['./version.component.scss'],
   template: `
-    <a href="https://github.com/SteveVanOpstal/LegendBuilder/releases" class="{{ pkg.version }}">
-      {{ pkg.version }}
+    <a href="https://github.com/SteveVanOpstal/LegendBuilder/releases" class="{{ version }}">
+      {{ version }}
     </a>`
 })
 
-export class VersionComponent { pkg = pkg; }
+export class VersionComponent { version = pkg.version; }
